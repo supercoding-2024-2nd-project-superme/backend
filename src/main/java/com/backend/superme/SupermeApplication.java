88efea1c6621d6553503731710e0625c3207d15e
Joojinsung <dev.jinsung1017@gmail.com>
2024-03-11 22:17:16 +0900

    기본설정

---
+++ src/main/java/com/backend/superme/SupermeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.superme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/superme/SupermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.superme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
