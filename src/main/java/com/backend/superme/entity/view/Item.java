e2391ac3d488bf1310b513e3d2f4a8d5587decc4
yoonipark <yoonipark09@gmail.com>
2024-03-12 21:48:05 +0900

    1st 엔티티 추가 Item & ItemStock 엔티티

---
+++ src/main/java/com/backend/superme/entity/view/Item.java
@@ -0,0 +1,64 @@
+package com.backend.superme.entity.view;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name ="item_name", nullable=false)
+    private String itemName;
+
+    @Column(name="main_img")
+    private String mainImg;
+
+    @Column(name = "descrption")
+    private String description;
+
+    @Column(name="color_option")
+    private String colorOption;
+
+    @Column(name="size_option")
+    private String sizeOption;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "stock_status")
+    private StockStatus stockStatus;
+
+    @Column(name = "price", precision = 10, scale = 2)
+    private BigDecimal price;
+
+    /*Todo :유저 연결
+    @ManyToOne
+    @JoinColumn(name = "seller",referencedColumnName = "id")
+    private User seller;
+    */
+
+   /* Todo : 카테고리 연결
+    @ManyToOne
+        @JoinColumn(name = "category", referencedColumnName = "id")
+    private Category category;
+    */
+    @Column(name = "registration_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date registrationDate;
+
+    @Column(name="termination_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date terminationDate;
+
+    public enum StockStatus {
+        IN_STOCK, OUT_OF_STOCK, BACK_ORDER
+    }
+
+}
